40733efc3701ec93d07d40efc2e0c08319e8663a
Chatnarint Boonsaeng <zenlektomyum@gmail.com>
2023-11-23 14:59:17 +0700

    feat: update

--- app/pages/auth/register/page.tsx
+++ app/pages/auth/register/page.tsx
@@ -1,25 +1,97 @@
+"use client";
 import { Button } from "@nextui-org/react";
+import { FormEvent, useState } from "react";
+import { basePath } from "@/app/core/settings";
+import Swal from "sweetalert2";
 
 export default function Register() {
+  const [userData, setUserData] = useState({
+    username: "",
+    password: "",
+    firstname: "",
+    lastname: "",
+    email: "",
+    status: "CUSTOMER"
+  })
+  const [conPass, setConPass] = useState("")
+
+  const onSubmit = async (event: FormEvent) => {
+    if (conPass !== userData.password) {
+      console.log(conPass);
+      console.log(userData.password);
+
+      return Swal.fire({
+        title: "ล้มเหลว",
+        text: "รหัสผ่านไม่ตรงกัน",
+        icon: "error",
+        confirmButtonText: "ตลลง",
+      });
+    }
+
+    try {
+      const response = await fetch(`${basePath}/php/register.php`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData),
+      });
+
+      if (response.ok) {
+        Swal.fire({
+          title: "สำเร็จ",
+          text: "เข้าสู่ระบบเสร็จสิ้น กด ตลลง เพื่อดำเนินการต่อ",
+          icon: "success",
+          confirmButtonText: "ตลลง",
+          confirmButtonColor: "#5CD1FF",
+        });
+        console.log('Data inserted successfully!');
+      } else {
+        Swal.fire({
+          title: "ล้มเหลว",
+          text: "รหัสผ่านไม่ตรงกัน",
+          icon: "error",
+          confirmButtonText: "ตลลง",
+        });
+        console.error('Failed to insert data.');
+      }
+    } catch (error) {
+      Swal.fire({
+        title: "ล้มเหลว",
+        text: "Error inserting data:" + error,
+        icon: "error",
+        confirmButtonText: "ตลลง",
+      });
+      console.error('Error inserting data:', error);
+    }
+  }
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setUserData({ ...userData, [event.target.name]: value });
+  };
+
   return (
-    <main className="h-screen flex flex-col justify-center items-center">
+    <main className="h-screen flex flex-col justify-center items-center pt-12">
       <h1 className="text-3xl">ลงทะเบียน</h1>
       <br />
       <form
-        action=""
-        method="GET"
+        // method="POST"
+        // action={`${basePath}/php/register.php`}
         className="bg-white shadow-md rounded-xl px-8 pt-6 pb-8 mb-4"
       >
         <div className="mb-4">
           <label
             className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="username"
           >
             ชื่อผู้ใช้งาน
           </label>
           <input
             className="shadow appearance-none border rounded-xl w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="username"
+            name="username"
+            value={userData.username}
+            onChange={handleChange}
             type="text"
             placeholder="Username"
             required
@@ -33,6 +105,9 @@ export default function Register() {
             <input
               className="shadow appearance-none border rounded-xl w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               id="password"
+              name="password"
+              value={userData.password}
+              onChange={handleChange}
               type="password"
               placeholder="Password"
               required
@@ -44,8 +119,10 @@ export default function Register() {
             </label>
             <input
               className="shadow appearance-none border rounded-xl w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="confirm_password"
-              type="confirm_password"
+              id="con_password"
+              name="con_password"
+              onChange={(e) => setConPass(e.target.value)}
+              type="password"
               placeholder="Confirm Password"
               required
             />
@@ -58,8 +135,10 @@ export default function Register() {
             </label>
             <input
               className="shadow appearance-none border rounded-xl w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="first_name"
-              type=" first_name"
+              id="firstname"
+              name="firstname"
+              value={userData.firstname}
+              onChange={handleChange}
               placeholder=" First Name"
               required
             />
@@ -70,8 +149,10 @@ export default function Register() {
             </label>
             <input
               className="shadow appearance-none border rounded-xl w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="last_name"
-              type="last_name"
+              id="lastname"
+              name="lastname"
+              value={userData.lastname}
+              onChange={handleChange}
               placeholder="Last Name"
               required
             />
@@ -87,13 +168,15 @@ export default function Register() {
           <input
             className="shadow appearance-none border rounded-xl w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="email"
-            type="text"
+            name="email"
+            value={userData.email}
+            onChange={handleChange}
             placeholder="Email"
             required
           />
         </div>
         <div className="flex items-center justify-center mt-8">
-          <Button color="danger" type="submit">
+          <Button color="danger" onClick={onSubmit}>
             สมัครสมาชิก
           </Button>
         </div>
--- next.config.js
+++ next.config.js
@@ -3,10 +3,14 @@ const isProd = process.env.NODE_ENV !== 'development'
 
 const nextConfig = {
     output: 'export',
+    trailingSlash: true,
     images: {
         unoptimized: true
     },
     basePath: isProd ? '/student/student65/u65301280005/mackerel-fish-shop-new' : undefined,
+    experimental: {
+        webpackBuildWorker: true
+    },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
